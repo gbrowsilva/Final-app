be6937c41ee602cfb8d781aab7ba46c19f1e7ac5
Gilmar Silva <gsilvabrow@gmail.com>
2018-11-27 22:34:47 -0200

    atualizando calculadora

--- app/src/main/java/com/lourdinas/appfinal/MainActivity.java
+++ app/src/main/java/com/lourdinas/appfinal/MainActivity.java
@@ -1,21 +1,55 @@
 package com.lourdinas.appfinal;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity implements
-        NavigationView.OnNavigationItemSelectedListener{
+        NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        } else {
+            super.onBackPressed();
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -27,11 +61,9 @@ public class MainActivity extends AppCompatActivity implements
                     "Minha configurações", Toast.LENGTH_SHORT).show();
 
             return true;
-
-
         }
-            return super.onOptionsItemSelected(item);
-        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
@@ -44,6 +76,10 @@ public class MainActivity extends AppCompatActivity implements
             Toast.makeText(getApplicationContext(),
                     "Abrir calculadora média", Toast.LENGTH_SHORT).show();
         }else if (id ==R.id.calculadora){
+
+            Intent intent = new Intent(MainActivity.this, Calculadora.class);
+            startActivity(intent);
+
             Toast.makeText(getApplicationContext(),
                     "Abrir Calculadora", Toast.LENGTH_SHORT).show();
         }
@@ -51,5 +87,7 @@ public class MainActivity extends AppCompatActivity implements
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
         drawerLayout.closeDrawer(GravityCompat.START);
         return false;
+
+
     }
 }
